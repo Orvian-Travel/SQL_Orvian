60d663298ee5c414c9f4f4b30ba05696096fc70f
DaltonGuima <dalton.guimaraes500@gmail.com>
2025-07-16 16:21:06 -0300

    Adicionando desconto no pix de 5%, na verifacação

--- Triggers/Orvian_Triggers.sql
+++ Triggers/Orvian_Triggers.sql
@@ -465,37 +465,37 @@ BEGIN
         INNER JOIN TB_RESERVATIONS R ON R.ID = I.ID_RESERVATION
         INNER JOIN TB_PACKAGES_DATES PD ON PD.ID = R.ID_PACKAGES_DATES
         INNER JOIN TB_PACKAGES P ON P.ID = PD.ID_PACKAGE
-        -- Subqueries diretas para descontos e quantidade de viajantes
         LEFT JOIN TB_PROMOTIONS PR1 ON PR1.ID = P.ID_PROMOTION
         LEFT JOIN TB_PROMOTIONS PR2 ON PR2.ID = I.ID_PROMOTION
-        -- Conta viajantes diretamente
         CROSS APPLY (
             SELECT COUNT(*) AS QTD_VIAJANTES
             FROM TB_TRAVELERS T
             WHERE T.ID_RESERVATION = R.ID
         ) QV
-        -- Calcula valor final
         WHERE
-            -- Valor esperado
             ABS(
-                I.VALUE_PAID - 
+                I.VALUE_PAID -
                 (
-                    -- preço do pacote × quantidade de viajantes
                     (P.PRICE * QV.QTD_VIAJANTES)
-                    -- aplica descontos (se houver)
-                    * (1 - (COALESCE(PR1.DISCOUNT_PERCENT, 0) + COALESCE(PR2.DISCOUNT_PERCENT, 0)) / 100.0)
-                    -- aplica taxa (se houver)
+                    * (1 
+                        - (
+                            COALESCE(PR1.DISCOUNT_PERCENT, 0)
+                            + COALESCE(PR2.DISCOUNT_PERCENT, 0)
+                            + CASE WHEN I.PAYMENT_METHOD = 'pix' THEN 5 ELSE 0 END
+                          ) / 100.0
+                      )
                     * (1 + COALESCE(I.TAX, 0) / 100.0)
                 )
             ) > 0.01
     )
     BEGIN
-        RAISERROR('O VALUE_PAID está diferente do valor calculado considerando promoções, taxa e quantidade de viajantes!', 16, 1)
+        RAISERROR('O VALUE_PAID está diferente do valor calculado considerando promoções, taxa, quantidade de viajantes e desconto Pix!', 16, 1)
         ROLLBACK TRANSACTION
         RETURN
     END
 END
 GO
+
 
 -- =========================================================================
 -- TRIGGER 20: TRG_VALIDATE_MAX_DISCOUNT
